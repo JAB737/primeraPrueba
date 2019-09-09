436a4fdd9e9bf9227e1d03b5a16763ecea812536
JAB <jorgeacostabello@hotmail.com>
2019-09-08 22:47:22 -0400

    probando forEach()

--- jsGibs.js
+++ jsGibs.js
@@ -4,4 +4,8 @@ document.getElementById("Titulo").innerHTML = "Lo Cambie por GIT";
 var pets = ["wakeman","maggie","sasha"];
 console.log(pets.join("---"));
 
+pets.forEach(mostrarMiMascota);
 
+function mostrarMiMascota(value){
+    console.log("Quiero a " + value);
+}
